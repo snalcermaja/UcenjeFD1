f81c2d3903113efbd10a41690bfe995588988a38
snalcermaja <snalcermaja2@gmail.com>
2026-01-27 18:51:17 +0100

    JavaScript Hello world

---
+++ 2_js/vjezba01/skripta.js
@@ -0,0 +1,13 @@
+console.info('Hello world 5');
+console.table(JSON.parse('{"name":"John", "age":30, "car":null}'));
+
+console.log('O\'Neal'); // \ ATLGR + Q
+console.log("O'Neal");
+
+console.log('
+    Hello 
+    world
+    6
+    ); //`ALTGR + 7
+
+    document.getElementById('ispis').innerHTML='<a href="#">Poveznica</a>';
