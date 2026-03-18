db90f21d1eb5cdf64360f54fe5f5ac72f28a9653
snalcermaja <snalcermaja2@gmail.com>
2026-03-18 18:50:40 +0100

    Prikaz podataka u tablici, formatiranje broja i korištenje ikona

--- 3_REACT/EdunovaAPP/src/pages/smjerovi/SmjerPregled.jsx
+++ 3_REACT/EdunovaAPP/src/pages/smjerovi/SmjerPregled.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react"
 import SmjerService from "../../services/smjerovi/SmjerService"
+import { Table } from "react-bootstrap"
+import { NumericFormat } from "react-number-format"
+import { GrValidate } from "react-icons/gr"
 
 export default function SmjerPregled(){
 
@@ -18,11 +21,45 @@ export default function SmjerPregled(){
 
     return(
         <>
-        <ul>
-            {smjerovi && smjerovi.map((smjer)=>(
-                <li>{smjer.naziv}</li>
-            ))}
-        </ul>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Naziv</th>
+                        <th>Trajanje</th>
+                        <th>Cijena</th>
+                        <th>Datum pokretanja</th>
+                        <th>Aktivan</th>
+                        <th>Akcija</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {smjerovi && smjerovi.map((smjer)=>(
+                        <tr>
+                            <td>{smjer.naziv}</td>
+                            <td>{smjer.trajanje}</td>
+                            <td>
+                                <NumericFormat 
+                                value={smjer.cijena}
+                                displayType={'text'}
+                                thousandSeparator='.'
+                                decimalSeparator=','
+                                suffix={'€'}
+                                decimalScale={2}
+                                fixedDecimalScale
+                                />
+                            </td>
+                            <td>{smjer.datumPokretanja}</td>
+                            <td>
+                                <GrValidate 
+                                size={25}
+                                color={smjer.aktivan ? 'green' : 'red'}
+                                />
+                            </td>
+                            <td></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
         </>
     )
 }
